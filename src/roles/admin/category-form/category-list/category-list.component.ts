b1df1b082c97efcc277db009244cafee03074f55
Varun Reddy <2464480@cognizant.com>
2026-01-12 10:04:19 +0530

    added admin components

--- src/roles/admin/category-form/category-list/category-list.component.ts
+++ src/roles/admin/category-form/category-list/category-list.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { CategoryFormComponent } from '../category-form/category-form.component';
-import { Category } from '../../../models/model';
-import { CategoryService } from '../../../services/category.service';
+import { CategoryFormComponent } from '../category-form.component';
+import { Category } from '../../../../models/model';
+import { CategoryService } from '../../../../services/category.service';
 
 @Component({
   selector: 'app-category-list',
@@ -17,7 +17,7 @@ export class CategoryListComponent implements OnInit {
   showForm = false;
   editingCategory: Category | null = null;
 
-  constructor(private categoryService: CategoryService) { }
+  constructor(private categoryService: CategoryService) {}
 
   ngOnInit(): void {
     this.categoryService.getAllCategories().subscribe((categories) => {
